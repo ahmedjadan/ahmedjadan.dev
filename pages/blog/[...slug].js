ae7bcbcdcc66e8e59dab3f137d9af7c983092b26
ahmedjadan <a.jadan7@gmail.com>
2021-10-01 00:47:10 +0200

    bio

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -42,7 +42,6 @@ export async function getStaticPaths({ locales, defaultLocale, availableLocales 
 
 export async function getStaticProps({ defaultLocale, locales, locale, params }) {
   const otherLocale = locale !== defaultLocale ? locale : 'en'
-  console.log("getStaticProps ~ otherLocale", otherLocale)
   const allPosts = await getAllFilesFrontMatter('blog', otherLocale)
   const post = await getFileBySlug('blog', params.slug.join('/'), otherLocale)
 
