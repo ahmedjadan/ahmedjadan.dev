1a4f51243180138b3382ba638be895136b84930c
ahmedjadan <a.jadan7@gmail.com>
2022-05-12 21:51:21 +0200

    toast

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
-import dynamic from 'next/dynamic'
+import React, { useEffect } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+import dynamic from 'next/dynamic';
 import useSWR from 'swr';
 import axios from 'axios';
 import useTranslation from 'next-translate/useTranslation';
@@ -6,19 +9,25 @@ import { getAllFilesFrontMatter } from '../lib/mdx';
 import InfoCard from '/src/components/InfoCard';
 import Layout from '/src/Layout/Layout';
 
-const Head = dynamic(() => import('/src/components/Head'))
-const FeaturedPost = dynamic(() => import('/src/components/FeaturedPost'))
-const Repos = dynamic(() => import('/src/components/Repos'))
-const FeaturedProjects = dynamic(() => import('/src/components/FeaturedProjects'))
-const PostCard = dynamic(() => import('/src/components/PostCard'))
+const Head = dynamic(() => import('/src/components/Head'));
+const FeaturedPost = dynamic(() => import('/src/components/FeaturedPost'));
+const Repos = dynamic(() => import('/src/components/Repos'));
+const FeaturedProjects = dynamic(() =>
+  import('/src/components/FeaturedProjects')
+);
+const PostCard = dynamic(() => import('/src/components/PostCard'));
 
-
-const fetcher = url => axios.get(url).then(res => res.data)
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 export default function Home({ posts }) {
-
+  useEffect(() => {
+    window.addEventListener('offline', () => toast.error('You are offline!'));
+    window.addEventListener('online', () =>
+      toast.success('Successfully Connected!')
+    );
+  }, []);
   const { data } = useSWR('/api/github/repos', fetcher);
-  const pinnedItems = data?.viewer?.pinnedItems?.edges?.map(({ node }) => node)
+  const pinnedItems = data?.viewer?.pinnedItems?.edges?.map(({ node }) => node);
 
   const { t } = useTranslation();
 
@@ -26,7 +35,8 @@ export default function Home({ posts }) {
   const sortedBlogs = posts
     .sort(
       (a, b) =>
-        Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt)))
+        Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
+    )
     .filter((p) => p.featured === false)
     .slice(0, 3);
 
@@ -36,14 +46,15 @@ export default function Home({ posts }) {
         image="https://www.ahmedjadan.dev/static/meta.png"
         description={t('common:bio')}
       />
+      <Toaster />
       <InfoCard />
 
       <div className="max-w-5xl mx-auto py-5 px-4">
         <div className="py-4 ">
-        <h1 className="text-lg md:text-3xl pb-2 font-bold text-gray-700 dark:text-gray-200">
-        {t('common:featured_posts')}
-        </h1>
-      </div>
+          <h1 className="text-lg md:text-3xl pb-2 font-bold text-gray-700 dark:text-gray-200">
+            {t('common:featured_posts')}
+          </h1>
+        </div>
         <div className="mx-auto grid md:grid-cols-6 grid-cols-1 gap-x-6 bg-gray-100/50 dark:bg-[#242731a1] rounded-md ">
           <div className="col-span-3">
             <FeaturedPost posts={posts} />
@@ -59,7 +70,9 @@ export default function Home({ posts }) {
       <FeaturedProjects />
       <div className="mx-auto px-4  max-w-5xl md:py-16 py-8">
         <div className="py-6 ">
-          <h1 className="text-lg md:text-3xl font-bold text-gray-700 dark:text-gray-200" >{t('common:repoTitle')} </h1>
+          <h1 className="text-lg md:text-3xl font-bold text-gray-700 dark:text-gray-200">
+            {t('common:repoTitle')}{' '}
+          </h1>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3  gap-4">
           {pinnedItems?.map((item, idx) => (
@@ -71,16 +84,13 @@ export default function Home({ posts }) {
   );
 }
 
-
-
 export async function getStaticProps({ locale, defaultLocale, locales }) {
-
   const otherLocale = locale !== defaultLocale ? locale : 'en';
   const posts = await getAllFilesFrontMatter('blog', otherLocale);
 
   return {
     props: {
-      posts
+      posts,
     },
   };
 }
